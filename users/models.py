26276a58da433f56bd72b4b02bed3df898e807bc
ningpop <craft1933@naver.com>
2020-09-07 00:09:45 +0900

    #3.3 Finishing User Model

--- users/models.py
+++ users/models.py
@@ -18,8 +18,26 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+
+    LANGUAGE_CHIOCES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KOR = "krw"
+
+    CURRENCY_CHIOCES = ((CURRENCY_USD, "USD"), (CURRENCY_KOR, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHIOCES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHIOCES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
