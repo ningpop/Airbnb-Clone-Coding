83f8c6ff97ad6a34e7ff3a448c6ce54ad9b8cb62
ningpop <craft1933@naver.com>
2020-09-07 19:34:56 +0900

    #3.4 Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,12 @@ from . import models
 
 # Register your models here.
 
-
+# admin.site.register(models.User, CustomUserAdmin) 과 같은 기능을 함
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    # admin 페이지에서 모델의 속성이 보임
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
